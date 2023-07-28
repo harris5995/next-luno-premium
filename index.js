729d333cbb8c4a78ea22e358d1ab5a4b05eb6a36
Harris Dzulkarnain <harisidzwan@gmail.com>
2023-07-28 14:39:46 +0800

    Part One Done

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-import { luno } from './luno.js'
-import { getBinance } from './binance.js'
-import { forex } from './forex.js'
+import { luno } from './lib/luno.js'
+import { getBinance } from './lib/binance.js'
+import { forex } from './lib/exchange-rate.js'
 
-const luno1 = await luno()
-console.log(`BTCMYR price on Luno: MYR`,luno1)
-
+const luno1 = await luno();
 const forex1 = await forex();
-console.log("USDMYR:", forex1)
-
 const binance1 = await getBinance();
-console.log(`BTCBUSD price on Binance: USD`,binance1)
-
 const lunoUSD = await lunotoUSD();
-console.log("BTCUSD price on Luno: USD", lunoUSD);
-
 const pD = await priceDifference();
-console.log(`Price Difference = USD`, pD);
-
 const lP = await lunoPremium();
-console.log(`Luno Premium = `, lP, `%`);
+
+console.log("BTCMYR price on Luno :  MYR", luno1)
+console.log("USDMYR : ", forex1)
+console.log("BTCBUSD price on Binance: USD", binance1)
+console.log("BTCUSD price on Luno: USD", lunoUSD);
+console.log("Price Difference : USD", pD);
+console.log("Luno Premium : ", lP, `%`);
 
 
 async function lunotoUSD() {
@@ -29,29 +25,32 @@ async function lunotoUSD() {
     return results;
 
   } catch (error) {
-    console.error('Error fetching data:', error);
+      console.error('Error fetching data:', error);
   }
 }
+
   async function priceDifference() {
      try {
-    const luno = lunoUSD;
-    const binance = binance1;
-    const result = luno - binance;
-    return result;
+      const luno = lunoUSD;
+      const binance = binance1;
+      const result = luno - binance;
+      return result;
+
      } catch (error) {
-       console.error('Error fetching data:', error);
+        console.error('Error fetching data:', error);
      }
    }
 
   async function lunoPremium() {
     try {
-  const Luno = lunoUSD;
-  const price = pD;
-  const ratio = price / Luno;
-  const premium = ratio * 100;
-  return premium;
+    const Luno = lunoUSD;
+    const price = pD;
+    const ratio = price / Luno;
+    const premium = ratio * 100;
+    return premium;
+
     } catch (error) {
-      console.error('Error fetching data:', error);
+        console.error('Error fetching data:', error);
     }
  }
 
