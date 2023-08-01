4609b8f7abe31350cd7d331d4b03f3b0f18993fd
Harris Dzulkarnain <harisidzwan@gmail.com>
2023-08-01 09:25:40 +0800

    latest update

---
+++ index3.js
@@ -0,0 +1,71 @@
+import { luno } from './lib/luno.js'
+import { getBinance } from './lib/binance.js'
+import { forex } from './lib/exchange-rate.js'
+import { lunotoUSD, priceDifference, lunoPremium } from './lib/math.js';
+
+
+// const luno1 = await luno();
+// const forex1 = await forex();
+// const binance1 = await getBinance();
+// const lunoUSD = await lunotoUSD();
+// const pD = await priceDifference();
+// const lP = await lunoPremium();
+
+export async function endResult() {
+
+const luno1 = await luno();
+const forex1 = await forex();
+const binance1 = await getBinance();
+const lunoUSD = await lunotoUSD();
+const pD = await priceDifference();
+const lP = await lunoPremium();
+
+console.log("BTCMYR price on Luno:".padEnd(30) + "MYR " + luno1)
+console.log("USDMYR:".padEnd(30) + forex1)
+console.log("BTCUSD price on Luno:".padEnd(30) + "USD " + lunoUSD);
+console.log("BTCBUSD price on Binance:".padEnd(30) + "USD " + binance1)
+console.log("Price difference:".padEnd(30) + "USD", pD);
+console.log("Luno premium:".padEnd(30) + lP.toFixed(4) + "%");
+}
+
+endResult()
+// async function lunotoUSD() {
+//   try {
+//     const lunoMYR = luno1;
+//     const usd = forex1;
+//     const results = lunoMYR / usd;
+//     return results;
+
+//   } catch (error) {
+//       console.error('Error fetching data:', error);
+//   }
+// }
+
+//   async function priceDifference() {
+//      try {
+//       const luno = lunoUSD;
+//       const binance = binance1;
+//       const result = luno - binance;
+//       return result;
+
+//      } catch (error) {
+//         console.error('Error fetching data:', error);
+//      }
+//    }
+
+//   async function lunoPremium() {
+//     try {
+//     const Luno = lunoUSD;
+//     const price = pD;
+//     const ratio = price / Luno;
+//     const premium = ratio * 100;
+//     return premium;
+
+//     } catch (error) {
+//         console.error('Error fetching data:', error);
+//     }
+//  }
+
+
+
+
--- test/integration.test.js
+++ test/integration.test.js
@@ -3,13 +3,13 @@ beforeEach(() => {
   });
 
 test("Returns expected display in console if everything is working", async () => {
-    const lunoResult = require("../index.js").lunoResult;
+    const endResult = require("../index3.js").endResult;
     const MOCK_LUNOPRICE = 1500000;
     jest.mock("../lib/luno.js", () => {
         
         const MOCK_LUNOPRICE = 1500000;
         return {
-            lunoPriceMYR() {
+            luno() {
                 return new Promise(res => res(MOCK_LUNOPRICE))
             }
         };
@@ -18,7 +18,7 @@ test("Returns expected display in console if everything is working", async () =>
     const MOCK_RATE = 4.5;
     jest.mock('../lib/exchange-rate.js', () => {
         return {
-            exchangeRate(){
+            forex(){
                 return new Promise(res => res(MOCK_RATE))
             }
         };
@@ -27,24 +27,64 @@ test("Returns expected display in console if everything is working", async () =>
     const MOCK_BINANCERATE = 2000000;
     jest.mock("../lib/binance.js", () => {
         return {
-            binancePriceUSD() {
+            getBinance() {
                 return new Promise(res => res(MOCK_BINANCERATE))
             }
         };
     })
 
-    const LUNO_USD = MOCK_LUNOPRICE / MOCK_RATE;
-    const PRICE_DIFFERENCE = LUNO_USD - MOCK_BINANCERATE;
-    const LUNO_PREMIUM = (PRICE_DIFFERENCE / LUNO_USD) * 100;
+    jest.mock('../lib/math.js', () => {
+        return {
+          lunotoUSD() {
+            const MOCK_LUNOUSD = 2500000;
+            return Promise.resolve(MOCK_LUNOUSD);
+          },
+          priceDifference() {
+            const PRICE_DIFFERENCE = 1;
+            return Promise.resolve(PRICE_DIFFERENCE);
+          },
+          lunoPremium() {
+            const LUNO_PREMIUM = 0.25;
+            return Promise.resolve(LUNO_PREMIUM);
+          },
+        };
+      });
+    
+    // const MOCK_LUNOUSD = 2500000;
+    // jest.mock("../lib/math.js", () => {
+    //     return {
+    //         lunotoUSD() {
+    //             return new Promise(res => res(MOCK_LUNOUSD))
+    //         }
+    //     };
+    // })
+
+    // const PRICE_DIFFERENCE = 1;
+    // jest.mock("../lib/math.js", () => {
+    //     return {
+    //         priceDifference() {
+    //             return new Promise(res => res(PRICE_DIFFERENCE))
+    //         }
+    //     };
+    // })
+
+    // const LUNO_PREMIUM = 0.25;
+    // jest.mock("../lib/math.js", () => {
+    //     return {
+    //         lunoPremium() {
+    //             return new Promise(res => res(LUNO_PREMIUM))
+    //         }
+    //     };
+    // })
 
     console.log = jest.fn(() => undefined)
 
-    await lunoResult();
+    await endResult();
 
     expect(console.log).toHaveBeenCalledwith('BTCMYR price on Luno: '.padEnd(30) + "MYR " + MOCK_LUNOPRICE);
     expect(console.log).toHaveBeenCalledwith('USDMYR:'.padEnd(30) + MOCK_RATE);
     expect(console.log).toHaveBeenCalledwith('BTCUSD price on Luno:'.padEnd(30) + "USD" + LUNO_USD);
-    expect(console.log).toHaveBeenCalledwith('BTCUSD price on Binance:'.padEnf(30) + 'USD' + MOCK_BINANCERATE);
+    expect(console.log).toHaveBeenCalledwith('BTCUSD price on Binance:'.padEnd(30) + 'USD' + MOCK_BINANCERATE);
     expect(console.log).toHaveBeenCalledwith('Price Difference:'.padEnd(30) + 'USD' + PRICE_DIFFERENCE);
     expect(console.log).toHaveBeenCalledwith('Luno Premium:'.padEnd(30) + LUNO_PREMIUM.toFixed(4) + "%")
 
